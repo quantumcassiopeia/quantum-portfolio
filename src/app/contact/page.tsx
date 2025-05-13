e57aaeebed2f77e00425af7bfb16b1f237e88d7b
Quantum <russewelt@gmail.com>
2025-05-13 18:33:35 -0300

    fix: AnimatedLottie render

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,16 @@
-import AnimatedLottie from "@/components/AnimatedLottie";
+import dynamic from "next/dynamic";
+
+const AnimatedLottie = dynamic(() => import("@/components/AnimatedLottie"), {
+  ssr: false,
+});
+
 import crossplatform from "../../../public/animations/crossplatform.json";
 
 export default function ContactPage() {
   return (
     <div>
-      Contato
-      <AnimatedLottie animationData={crossplatform} />
+      <h1>Contato</h1>
+      <AnimatedLottie animationData={crossplatform} speed={0.5} width="30%" />
     </div>
   );
 }
