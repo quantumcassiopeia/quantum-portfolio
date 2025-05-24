cefd504152ae47ab35a2cfa935e680949bde6ec9
Quantum <russewelt@gmail.com>
2025-05-24 19:15:22 -0300

    added: UnderMaintenance Component

--- src/app/cases/[slugs]/page.tsx
+++ src/app/cases/[slugs]/page.tsx
@@ -1,7 +1,7 @@
 export default function CasesLayout() {
   return (
     <div>
-      <h1>Case</h1>
+      <h1>Case id</h1>
     </div>
   );
 }
---
+++ src/app/cases/page.tsx
@@ -0,0 +1,9 @@
+import UnderMaintenance from "@/components/UnderMaintenance";
+
+export default function CasesPage() {
+  return (
+    <div>
+      <UnderMaintenance />
+    </div>
+  );
+}
