8fd7e27950006134e94487b67d0d4bd27ae816df
Quantum <russewelt@gmail.com>
2025-05-26 16:54:15 -0300

    added: useTransition

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,29 +1,36 @@
 "use client";
 
 import { usePathname, useRouter } from "@/i18n/navigation";
-
 import TranslateIcon from "@mui/icons-material/Translate";
+import { useTransition } from "react";
 
 export default function LocaleSwitcher() {
   const router = useRouter();
   const pathname = usePathname();
+  const [, startTransition] = useTransition();
+
+  const handleLocaleChange = (locale: string) => {
+    startTransition(() => {
+      router.replace(pathname, { locale });
+    });
+  };
 
   return (
-    <div className="group relative p-3 flex flex-col items-center ">
+    <div className="group relative p-3 flex flex-col items-center">
       <TranslateIcon className="cursor-pointer" />
       <div
         className="absolute top-full flex flex-col gap-y-2 p-3 items-start backdrop-blur-sm bg-[var(--bg-color)]/40 shadow-lg rounded-2xl text-[1rem]
-    opacity-0 scale-95 pointer-events-none transition-all duration-700 ease-in-out
-    group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto"
+        opacity-0 scale-95 pointer-events-none transition-all duration-700 ease-in-out
+        group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto"
       >
         <button
-          onClick={() => router.replace(pathname, { locale: "en" })}
+          onClick={() => handleLocaleChange("en")}
           className="cursor-pointer focus:outline-none focus:ring focus:ring-blue-400"
         >
           English
         </button>
         <button
-          onClick={() => router.replace(pathname, { locale: "pt-BR" })}
+          onClick={() => handleLocaleChange("pt-BR")}
           className="cursor-pointer focus:outline-none focus:ring focus:ring-blue-400"
         >
           Português
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,10 +3,6 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "pt-BR"],
   defaultLocale: "en",
-  localeCookie: {
-    name: "USER_LOCALE",
-    maxAge: 60 * 60 * 24 * 365,
-  },
   pathnames: {
     "/": "/",
     "/pathnames": {
