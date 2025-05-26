fb004e5a913d6f4abf5455c47d7c49750fd73dbb
Quantum <russewelt@gmail.com>
2025-05-26 16:42:32 -0300

    added: Cookie structure

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,10 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "pt-BR"],
   defaultLocale: "en",
+  localeCookie: {
+    name: "USER_LOCALE",
+    maxAge: 60 * 60 * 24 * 365,
+  },
   pathnames: {
     "/": "/",
     "/pathnames": {
