296fc4f7fa0b1554c2441cc62163ca2c923581e3
Quantum <russewelt@gmail.com>
2025-05-26 17:28:45 -0300

    added: handleLocaleChange function

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -2,26 +2,22 @@
 
 import { usePathname, useRouter } from "@/i18n/navigation";
 import TranslateIcon from "@mui/icons-material/Translate";
-import { useTransition } from "react";
 
 export default function LocaleSwitcher() {
   const router = useRouter();
   const pathname = usePathname();
-  const [, startTransition] = useTransition();
 
   const handleLocaleChange = (locale: string) => {
-    startTransition(() => {
-      router.replace(pathname, { locale });
-    });
+    router.replace(pathname, { locale });
   };
 
   return (
-    <div className="group relative p-3 flex flex-col items-center">
+    <div className="group relative p-3 flex flex-col items-center ">
       <TranslateIcon className="cursor-pointer" />
       <div
         className="absolute top-full flex flex-col gap-y-2 p-3 items-start backdrop-blur-sm bg-[var(--bg-color)]/40 shadow-lg rounded-2xl text-[1rem]
-        opacity-0 scale-95 pointer-events-none transition-all duration-700 ease-in-out
-        group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto"
+    opacity-0 scale-95 pointer-events-none transition-all duration-700 ease-in-out
+    group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto"
       >
         <button
           onClick={() => handleLocaleChange("en")}
