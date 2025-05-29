967f3069da0126ef3a4e0fcbb120aee7d239da11
Quantum <russewelt@gmail.com>
2025-05-29 18:58:51 -0300

    refactor: Header Style

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,72 +3,49 @@
 import LocaleSwitcher from "@/components/LocaleSwitcher";
 import HamburgerMenu from "@/components/HamburgerMenu";
 import Navbar from "@/components/Navbar";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [isDesktop, setIsDesktop] = useState(false);
-  const [showHeader, setShowHeader] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
+  const lastScrollY = useRef(0);
+  const [isMobileHeaderHidden, setIsMobileHeaderHidden] = useState(false);
+  const [isAtTop, setIsAtTop] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
 
-      setIsScrolled(currentScrollY > 0);
-
-      if (currentScrollY < lastScrollY || currentScrollY < 10) {
-        setShowHeader(true);
+      if (lastScrollY.current < currentScrollY && currentScrollY > 10) {
+        setIsMobileHeaderHidden(true);
       } else {
-        setShowHeader(false);
+        setIsMobileHeaderHidden(false);
       }
 
-      setLastScrollY(currentScrollY);
+      setIsAtTop(currentScrollY < 10);
+      lastScrollY.current = currentScrollY;
     };
 
     window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [lastScrollY]);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth > 768);
-    };
-
-    handleResize(); // Chama inicialmente
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-
-  const backgroundColor = isScrolled
-    ? "shadow-md bg-[var(--bg-color)]"
-    : "bg-transparent";
 
   return (
     <header
-      className={`fixed h-16 md:h-20 left-0 right-0 top-0 z-50 transition-all duration-700 ease-in-out ${backgroundColor} ${
-        showHeader ? "" : isDesktop ? "" : "translate-y-[-100%]"
-      }`}
+      className={`fixed shadow-md bg-[var(--bg-color)] h-16 md:h-20 left-0 right-0 top-0 z-50 transition-all duration-500 ease-in-out ${
+        isAtTop ? "bg-transparent shadow-none" : ""
+      } ${isMobileHeaderHidden ? "-translate-y-full" : ""} md:-translate-y-0`}
     >
       <div className="flex items-center justify-between max-w-screen-2xl w-full mx-auto md:pr-4 h-full">
-        <div className="h-10 w-10 rounded-full  ml-4"></div>
+        <div className="h-10 w-10 rounded-full ml-4"></div>
         <div className="flex items-center gap-x-3">
           <LocaleSwitcher />
 
           <div className="flex gap-5 items-center">
-            {isDesktop ? (
-              <Navbar
-                className="flex gap-4"
-                showHome={false}
-                showThemeSwitcher
-              />
-            ) : (
-              <HamburgerMenu />
-            )}
+            <Navbar
+              className="hidden md:flex gap-4"
+              showHome={false}
+              showThemeSwitcher
+            />
+            <HamburgerMenu className="md:hidden" />
           </div>
         </div>
       </div>
