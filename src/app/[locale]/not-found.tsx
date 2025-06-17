7fa6a10ec5242c265c784d8a7c2d31f1ebdf127d
Quantum <russewelt@gmail.com>
2025-06-16 21:53:18 -0300

    added: lottie animation to notfound page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,7 +1,9 @@
 import { useTranslations } from "next-intl";
+import AnimatedLottie from "@/components/AnimatedLottie";
 import Button from "@/components/Button";
 import Link from "next/link";
 
+import notfound from "../../../public/animations/404.json";
 export default function NotFound() {
   const t = useTranslations("NotFound");
 
@@ -10,6 +12,7 @@ export default function NotFound() {
       <h1 className="text-5xl">{t("title")}</h1>
       <h2 className="text-2xl">{t("subtitle")}</h2>
       <p className="text-center max-w-92">{t("paragraph")}</p>
+      <AnimatedLottie animationData={notfound} />
       <Link href="/" className="text-center max-w-92">
         <Button className="mt-4">{t("button")}</Button>
       </Link>
