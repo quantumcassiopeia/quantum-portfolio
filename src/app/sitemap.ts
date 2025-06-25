78c9c1bc46f46a3c4d77cb3e21049b171a534187
Quantum <russewelt@gmail.com>
2025-06-25 17:43:22 -0300

    test: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,40 +1,26 @@
 import { MetadataRoute } from "next";
-import { Locale } from "next-intl";
-import { getPathname } from "@/i18n/navigation";
-import { routing } from "@/i18n/routing";
-
-const caseSlugs = ["1", "2", "3", "4", "5"];
 
 const host = "https://quantum-portfolio-alpha.vercel.app";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    ...getEntries("/"),
-    ...getEntries("/about-us"),
-    ...getEntries("/cases"),
-    ...getEntries("/contact"),
-    ...getDynamicCaseEntries(caseSlugs),
-  ];
-}
-
-type Href = Parameters<typeof getPathname>[0]["href"];
-
-function getEntries(href: Href) {
-  return routing.locales.map((locale) => ({
-    url: getUrl(href, locale),
-    alternates: {
-      languages: Object.fromEntries(
-        routing.locales.map((cur) => [cur, getUrl(href, cur)])
-      ),
+    {
+      url: `${host}/en`,
+      alternates: {
+        languages: {
+          en: `${host}/en`,
+          "pt-BR": `${host}/pt-BR`,
+        },
+      },
     },
-  }));
-}
-
-function getDynamicCaseEntries(slugs: string[]) {
-  return slugs.flatMap((slug) => getEntries(`/cases/${slug}`));
-}
-
-function getUrl(href: Href, locale: Locale) {
-  const pathname = getPathname({ locale, href });
-  return host + pathname;
+    {
+      url: `${host}/en/about-us`,
+      alternates: {
+        languages: {
+          en: `${host}/en/about-us`,
+          "pt-BR": `${host}/pt-BR/about-us`,
+        },
+      },
+    },
+  ];
 }
